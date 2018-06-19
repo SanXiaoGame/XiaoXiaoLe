7ccce14f85333bbda18d1eee5b84677a514030a2
LANOU-YF-\Administrator <lokj2018@sina.com>
2018-06-19 15:55:34 +0800

    结算计分加特殊块的累加

--- Assets/Sprites/Game/SpecialBlockObject.cs
+++ Assets/Sprites/Game/SpecialBlockObject.cs
@@ -36,6 +36,13 @@ public class SpecialBlockObject : MonoBehaviour
     /// <param name="eventData"></param>
     private void BlockOnPointerDown(PointerEventData eventData)
     {
+        //清零临时记录
+        Berserker = 0;
+        Caster = 0;
+        Hunter = 0;
+        Knight = 0;
+        Saber = 0;
+
         if (!GameManager.Instance.isBusy)
         {
             if (myPlayingObject.tag == ConstData.SkillBlock)
@@ -47,8 +54,28 @@ public class SpecialBlockObject : MonoBehaviour
                     if (myPlayingObject.adjacentItems[i] != null && myPlayingObject.adjacentItems[i].gameObject.activeSelf)
                     {
                         myPlayingObject.adjacentItems[i].brust = true;
+                        //记录消除的块数量
+                        switch (myPlayingObject.adjacentItems[i].name)
+                        {
+                            case ConstData.Berserker:
+                                Berserker++;
+                                break;
+                            case ConstData.Caster:
+                                Caster++;
+                                break;
+                            case ConstData.Hunter:
+                                Hunter++;
+                                break;
+                            case ConstData.Knight:
+                                Knight++;
+                                break;
+                            case ConstData.Saber:
+                                Saber++;
+                                break;
+                        }
                     }
                 }
+                //计分
                 GameManager.Instance.AddScore(ConstData.BlastSkill);
             }
             else if(myPlayingObject.tag == ConstData.SpecialBlock)
@@ -59,9 +86,93 @@ public class SpecialBlockObject : MonoBehaviour
                     for (int j = 0; j < ColumnManager.Instance.numberOfRows; j++)
                     {
                         ColumnManager.Instance.gameColumns[i].BlockObjectsScriptList[j].brust = true;
+                        //记录消除的块数量
+                        switch (myPlayingObject.adjacentItems[i].name)
+                        {
+                            case ConstData.Berserker:
+                                Berserker++;
+                                break;
+                            case ConstData.Caster:
+                                Caster++;
+                                break;
+                            case ConstData.Hunter:
+                                Hunter++;
+                                break;
+                            case ConstData.Knight:
+                                Knight++;
+                                break;
+                            case ConstData.Saber:
+                                Saber++;
+                                break;
+                        }
                     }
                 }
+
+                #region 全屏块的最终技能
+                if (Berserker >= 3)
+                {
+                    Berserker = 3;
+                    if (Berserker >= 4)
+                    {
+                        Berserker = 4;
+                        if (Berserker >= 5)
+                        {
+                            Berserker = 5;
+                        }
+                    }
+                }
+                if (Caster >= 3)
+                {
+                    Caster = 3;
+                    if (Caster >= 4)
+                    {
+                        Caster = 4;
+                        if (Caster >= 5)
+                        {
+                            Caster = 5;
+                        }
+                    }
+                }
+                if (Hunter >= 3)
+                {
+                    Hunter = 3;
+                    if (Hunter >= 4)
+                    {
+                        Hunter = 4;
+                        if (Hunter >= 5)
+                        {
+                            Hunter = 5;
+                        }
+                    }
+                }
+                if (Knight >= 3)
+                {
+                    Knight = 3;
+                    if (Knight >= 4)
+                    {
+                        Knight = 4;
+                        if (Knight >= 5)
+                        {
+                            Knight = 5;
+                        }
+                    }
+                }
+                if (Saber >= 3)
+                {
+                    Saber = 3;
+                    if (Saber >= 4)
+                    {
+                        Saber = 4;
+                        if (Saber >= 5)
+                        {
+                            Saber = 5;
+                        }
+                    }
+                }
+                #endregion
+
             }
+            //计分
             GameManager.Instance.AddScore(ConstData.SpecialSkill);
             transform.DOScale(Vector3.zero, 0.34f).OnComplete(delegate ()
             {
