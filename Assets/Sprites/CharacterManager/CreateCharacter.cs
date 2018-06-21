385a7289066dd11c2dc0bc4d704caa6e8a4bd68f
lin <1670641435@qq.com>
2018-06-21 16:07:54 +0800

    角色管理界面

---
+++ Assets/Sprites/CharacterManager/CreateCharacter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CreateCharacter : MonoBehaviour {
+    
+
+    GameObject target;
+    private void Awake()
+    {
+
+        foreach (var item in SQLiteManager.Instance.characterDataSource)
+        {
+           
+            AddCharacter(item.Key);
+           
+        }
+    }
+    public void AddCharacter(int ID) {
+        target = Resources.Load("Prefabs/PlayerPrefabs/player") as GameObject;
+        GameObject character = Instantiate(target);
+        character.transform.SetParent(this.transform);
+        character.transform.localPosition = Vector3.zero;
+        this.gameObject.GetComponent<RectTransform>().sizeDelta = new Vector2(this.gameObject.GetComponent<RectTransform>().sizeDelta.x, this.gameObject.GetComponent<RectTransform>().sizeDelta.y + this.gameObject.GetComponent<GridLayoutGroup>().cellSize.y + this.gameObject.GetComponent<GridLayoutGroup>().spacing.y);
+        transform.position = new Vector3(transform.position.x, transform.position.y - 30, transform.position.z);
+        Character _character = character.AddComponent<Character>();
+        _character.Name.text = SQLiteManager.Instance.characterDataSource[ID].character_Name;
+        _character.Lv.text = SQLiteManager.Instance.characterDataSource[ID].character_Level.ToString();
+        string path = string.Format("Texture/Icon0/Class_{0}",SQLiteManager.Instance.characterDataSource[ID].character_Class);
+        _character.HeadPortrait.sprite = Resources.Load(path, typeof(Sprite)) as Sprite;
+        _character.ID = ID;
+        _character.Type = SQLiteManager.Instance.characterDataSource[ID].character_Class;
+
+        SQLiteManager.Instance.characterLists.Add(character);
+    }
+}
---
+++ Assets/Sprites/CharacterManager/SelectBtn.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SelectBtn : MonoBehaviour {
+    public string characterName;
+    public int ID { get; set; }
+    Transform[] pos = new Transform[5];
+    private void Awake()
+    {
+        
+        GameObject[] gameObject = GameObject.FindGameObjectsWithTag("Charater1");
+        for (int i = 0; i < pos.Length; i++)
+        {
+            pos[i] = gameObject[i].transform;
+        }
+       
+        GameObject target = Resources.Load("Prefabs/PlayerPrefabs/HeadPortrait") as GameObject;
+
+        bool isOver = false;
+        bool isCome = false;
+        this.transform.Find("Submit").GetComponent<Button>().onClick.AddListener(()=> {
+            for (int i = 0; i < pos.Length; i++)
+            {
+                if (pos[i].childCount == 0)
+                {
+                    isOver = true;
+                }
+            }
+           // Debug.Log(!SQLiteManager.Instance.character.ContainsKey(SQLiteManager.Instance.characterDataSource[ID].character_Class));
+            if (ID != 0&&isOver& !SQLiteManager.Instance.character.ContainsKey(SQLiteManager.Instance.characterDataSource[ID].character_Class))
+            {
+                
+                string path = string.Format("Texture/Icon0/{0}", SQLiteManager.Instance.characterDataSource[ID].character_Class);
+                Sprite sprite = Resources.Load(path, typeof(Sprite)) as Sprite;
+                GameObject @object = Instantiate(target);
+                @object.GetComponent<Image>().sprite = sprite;
+                @object.name = sprite.name;
+                HeadPortrait headPortrait = @object.AddComponent<HeadPortrait>();
+                headPortrait.ID = ID;
+                for (int i = 0; i < pos.Length; i++)
+                {
+                    if (pos[i].childCount == 0)
+                    {
+                        @object.transform.parent = pos[i];
+                        @object.transform.localPosition = Vector3.zero;
+                    }
+                }
+                SQLiteManager.Instance.character.Add(SQLiteManager.Instance.characterDataSource[ID].character_Class, ID);
+                isCome = true;
+            }
+           
+        });
+        this.transform.Find("Delet").GetComponent<Button>().onClick.AddListener(() => {
+            if (isCome)
+            {
+                SQLiteManager.Instance.character.Remove(GameObject.Find(characterName).GetComponent<Image>().sprite.name);
+                Destroy(GameObject.Find(characterName));
+                isCome = false;
+            }
+            
+
+        });
+        this.transform.Find("Centain").GetComponent<Button>().onClick.AddListener(() => {
+         
+        });
+    }
+}
