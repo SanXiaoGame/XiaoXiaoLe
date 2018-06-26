28215c53f28ebb3cb1446c1fc8384728f4bda63c
LANOU-YF-\Administrator <lokj2018@sina.com>
2018-06-26 14:14:45 +0800

    新增加载场景，新增场景管理类，新增加载预制体，修改资源管理类

---
+++ Assets/Sprites/ToolClass/GameStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStart : MonoBehaviour
+{
+    /// <summary>
+    /// 添加所需的管理类
+    /// </summary>
+    private void Awake()
+    {
+        //添加SQLiteManager
+        GameObject obj = new GameObject(typeof(SQLiteManager).Name);
+        obj.AddComponent<SQLiteManager>();
+        DontDestroyOnLoad(obj);
+        // 启用加载界面预制体
+        UIManager.Instance.PushUIStack(ConstData.LoadingPrefab);
+    }
+}
