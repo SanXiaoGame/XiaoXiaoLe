a5ba031f142b19b7ff97c0c2a10a7c271e6b0f20
LANOU-YF-\Administrator <lokj2018@sina.com>
2018-06-26 14:14:45 +0800

    新增加载场景，新增场景管理类，新增加载预制体，修改资源管理类

---
+++ Assets/Sprites/Manager/SceneAss_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 所有场景管理类
+/// </summary>
+public class SceneAss_Manager : ManagerBase<SceneAss_Manager>
+{
+    //启用对应场景的事件
+    internal event Action<string> readDataEnd;
+
+    /// <summary>
+    /// 启用协程
+    /// </summary>
+    /// <param 场景名="name"></param>
+    internal void ExecutionOfEvent(string name)
+    {
+        readDataEnd(name);
+    }
+}
