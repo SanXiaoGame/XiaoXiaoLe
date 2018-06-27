287ea78eae023e878a3ef56bce38904c54a7e529
LANOU-YF-\Administrator <lokj2018@sina.com>
2018-06-27 16:00:25 +0800

    更换安卓和PC通用的数据库动态库，修改手机测试工具类，删除测试用打印

--- Assets/Sprites/Manager/SQLiteManager.cs
+++ Assets/Sprites/Manager/SQLiteManager.cs
@@ -36,7 +36,7 @@ public class SQLiteManager : ManagerBase<SQLiteManager>
         //是否拷贝完成
         StreamsLoading.onCopyFinished += OnLoadFinished;
         //开始拷贝（输入对应的表名）
-        StreamsLoading.LoadWitgPath(new string[] { ConstData.SQLITE_NAME});
+        StreamsLoading.LoadWitgPath(ConstData.SQLITE_NAME);
     }
 
     /// <summary>
@@ -47,7 +47,8 @@ public class SQLiteManager : ManagerBase<SQLiteManager>
     {
         Debug.Log("路径拷贝完成");
         //数据库存放沙盒的路径
-        dataBasePath = System.IO.Path.Combine(Application.persistentDataPath, ConstData.SQLITE_NAME);
+        //dataBasePath = System.IO.Path.Combine(Application.persistentDataPath, ConstData.SQLITE_NAME);
+        dataBasePath = StringSplicingTool.StringSplicing(new string[] { Application.persistentDataPath,"/", ConstData.SQLITE_NAME });
         //初始化读和写的功能
         readData = new ReadData(dataBasePath);
         writeData = new WriteData(dataBasePath);
@@ -71,6 +72,7 @@ public class SQLiteManager : ManagerBase<SQLiteManager>
         readData.GetData(ConstData.Skill);
         readData.GetData(ConstData.State);
         readData.GetData(ConstData.Player);
+        print("获取完数据");
         //执行读取结束委托
         SceneAss_Manager.Instance.ExecutionOfEvent("TestScene");
     }
