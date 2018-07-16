9a20a8ed84cf980c1b178a72986f24ac80818c99
LANOU-YF-\Administrator <TsukiyomiC12@Gmail.com>
2018-07-16 14:43:26 +0800

    角色管理界面，新增一些常量和更改部分脚本

---
+++ Assets/Sprites/UI/Bag/BagItem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BagItem : MonoBehaviour
+{
+    internal EquipmentData mydata_equipt;
+    internal ItemData mydata_item;
+    //所属格子
+    internal int myGrid;
+
+    internal void GetData()
+    {
+        if (transform.tag == ConstData.EquipmentType)
+        {
+            mydata_equipt = SQLiteManager.Instance.equipmentDataSource[Convert.ToInt32(transform.name)];
+        }
+        else if (transform.tag == ConstData.ItemType)
+        {
+            mydata_item = SQLiteManager.Instance.itemDataSource[Convert.ToInt32(transform.name)];
+        }
+    }
+}
