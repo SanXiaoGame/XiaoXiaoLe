5406cf1a0a159d8fee2440050e1b6aba11bee86c
LANOU-YF-\Administrator <lokj2018@sina.com>
2018-07-18 17:58:41 +0800

    修改UI的激活赋值

--- Assets/Sprites/Manager/UIManager.cs
+++ Assets/Sprites/Manager/UIManager.cs
@@ -6,11 +6,35 @@ using UnityEngine;
 /// </summary>
 public class UIManager : ManagerBase<UIManager>
 {
+    //游戏暂停开关
+    bool isGamePause = false;
+
     Stack<IUIBase> UIStack = new Stack<IUIBase>();
     //保存所有进栈的UI界面
     Dictionary<string, IUIBase> CurrentUI = new Dictionary<string, IUIBase>();
     //所有UI的父级画布
     Transform uiParent;
+
+    /// <summary>
+    /// 检测退出按钮的点击
+    /// </summary>
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            //显示退出界面
+            PushUIStack(ConstData.ExitPrefab);
+        }
+    }
+
+    /// <summary>
+    /// 游戏暂停
+    /// </summary>
+    internal void GamePause()
+    {
+        isGamePause = !isGamePause;
+        Time.timeScale = isGamePause == true ? 0 : 1;
+    }
 
     /// <summary>
     /// 实例化界面,并返回当前界面
